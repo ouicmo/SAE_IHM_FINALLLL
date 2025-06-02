b054dd900dd6de86755741500f1f9ab0a0dff073
ahach <ahachicha4@outlook.fr>
2025-06-02 21:04:14 +0200

    ajout javadoc

--- src/main/java/appli/modele/Scenario.java
+++ src/main/java/appli/modele/Scenario.java
@@ -51,25 +51,3 @@ public class Scenario {
         return vendeurs;
     }
 }
-
-
-
-    /*public Map<String, String> lectureScenario(String scenario) throws FileNotFoundException {
-        //Lecture des scénarios du fichier scénario.txt permettant d'obtenir un dictionnaire transactions tel que (Vendeur,Acheteur)
-        Map<String,String> scenarios = new LinkedHashMap<>();
-        for (int i=0;i<9;i++) {
-            scenarios.put("s"+i,"scenario_"+i);
-        }
-        File scenarioFile = new File("src/Données/"+scenarios.get(scenario)+".txt");
-        Scanner scan3 = new Scanner(scenarioFile);
-
-        Map<String, String> transactions = new LinkedHashMap<>();
-
-        while (scan3.hasNextLine()) {
-            String line = scan3.nextLine();
-            String[] split = line.split(" -> ");
-            transactions.put(split[0], split[1]);
-        }
-        return transactions;
-    }*/
-
