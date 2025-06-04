adacabe05a8a2d552deba354f62179908f501e61
ouicmo <109861938+ouicmo@users.noreply.github.com>
2025-06-04 11:43:55 +0200

    Update MembreTest.java

--- src/test/java/appli/modele/MembreTest.java
+++ src/test/java/appli/modele/MembreTest.java
@@ -1,5 +1,6 @@
 package appli.modele;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -8,6 +9,10 @@ import static org.junit.jupiter.api.Assertions.*;
  * Tests unitaires pour la classe {@link Membre}.
  */
 public class MembreTest {
+
+
+
+
 
     @Test
     void testConstructeurEtGetChVille() throws Exception {
@@ -26,4 +31,45 @@ public class MembreTest {
             new Membre("UtilisateurInconnu");
         });
     }
+
+    @Test
+    void testEqualsIdentique() throws Exception {
+        // Test pour vérifier que deux membres avec le même pseudonyme sont égaux
+        Membre membre1Bis = new Membre("Psykokwak");
+        Membre membre1 = new Membre("Psykokwak");
+        assertTrue(membre1.equals(membre1Bis), "Les membres avec le même pseudonyme doivent être égaux");
+    }
+
+    @Test
+    void testEqualsDifferent() throws Exception {
+        // Test pour vérifier que deux membres avec des pseudonymes différents ne sont pas égaux
+        Membre membre1 = new Membre("Psykokwak");
+        Membre membre2 = new Membre("Machoc");
+        assertFalse(membre1.equals(membre2), "Les membres avec des pseudonymes différents ne doivent pas être égaux");
+    }
+
+    @Test
+    void testHashCodeIdentique() throws Exception {
+        // Test pour vérifier que deux membres avec le même pseudonyme ont le même code de hachage
+        Membre membre1Bis = new Membre("Psykokwak");
+        Membre membre1 = new Membre("Psykokwak");
+        assertEquals(membre1.hashCode(), membre1Bis.hashCode(), "Les membres avec le même pseudonyme doivent avoir le même hashCode");
+    }
+
+    @Test
+    void testHashCodeDifferent() throws Exception {
+        // Test pour vérifier que deux membres avec des pseudonymes différents ont des codes de hachage différents
+        Membre membre1 = new Membre("Psykokwak");
+        Membre membre2 = new Membre("Machoc");
+        assertNotEquals(membre1.hashCode(), membre2.hashCode(), "Les membres avec des pseudonymes différents doivent avoir des hashCode différents");
+    }
+
+    @Test
+    void testToString() throws Exception {
+        // Test pour vérifier que la méthode toString retourne le pseudonyme correct
+        Membre membre1 = new Membre("Psykokwak");
+        assertEquals("Psykokwak", membre1.toString(), "La méthode toString doit retourner le pseudonyme du membre");
+    }
+
+
 }
